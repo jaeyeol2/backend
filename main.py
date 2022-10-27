334fd18212355abcbefeb8d00b33bdd0b55267c4
bjy <woduf5123@gmail.com>
2022-10-27 15:07:43 +0900

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ cors = CORS(app, resources={r"/*":{"origins": "*"}})
 
 @app.route("/hello")
 def hello():
-    result = {"code" : 200, "message":"hello flank"}
+    result = {"code" : 200, "message":"hello test1"}
     return result
 
 if __name__ == "__main__":
