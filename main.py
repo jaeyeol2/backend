bc38539246f66f4698dd110191a51b5f2b812878
bjy <woduf5123@gmail.com>
2022-10-27 13:37:09 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*":{"origins": "*"}})
+
+@app.route("/hello")
+def hello():
+    result = {"code" : 200, "message":"hello flank"}
+    return result
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=9777)
